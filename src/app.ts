7b831287efa88c169aa5d38f30875e2974a3ddf6
rakibaakter <rakibaakter635@gmail.com>
2024-05-23 13:09:25 +0600

    set up prettier for auto fix format

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import express, { Request, Response } from "express"
-import cors from "cors"
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
-const app = express()
-
-
+const app = express();
 
 // parsers
 app.use(express.json());
-app.use(cors())
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,15 @@
-import app from "./app"
-import mongoose from "mongoose";
-import config from "./app/config";
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config';
 
 async function main() {
-    await mongoose.connect(config.databaseUrl as string);
-        try {
-            app.listen(config.port, () => {
-                console.log(`server listening on port ${config.port}`)
-              })
-        } catch (error) {
-            console.log(error);
-            
-        }
-
-
-} 
+  await mongoose.connect(config.databaseUrl as string);
+  try {
+    app.listen(config.port, () => {
+      console.log(`server listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 main();
-
